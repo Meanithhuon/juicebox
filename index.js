61b494dba6f964ca094e99feb916bd1ed31346dd
Meanithhuon <huon.meanith@gmail.com>
2023-01-23 00:06:36 -0600

    11th commit

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const PORT = 3000;
 const express = require('express');
 const server = express();
+const morgan = require('morgan');
+const apiRouter = require('./api');
+
+server.use(morgan('dev'));
+server.use(express.json())
+server.use('/api', apiRouter);
 
 const { client } = require('./db');
 client.connect();
@@ -15,10 +21,8 @@ server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
 });
 
-const morgan = require('morgan');
-server.use(morgan('dev'));
 
-server.use(express.json())
+
 
 server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
@@ -30,5 +34,4 @@ server.use((req, res, next) => {
 
 
 
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
+
