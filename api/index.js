5a40b7ba3940a03180f0afc1640be6340c4d8696
Meanithhuon <huon.meanith@gmail.com>
2023-01-31 17:44:14 -0600

    check for malformed token

--- api/index.js
+++ api/index.js
@@ -7,15 +7,24 @@ const { JWT_SECRET } = process.env;
 apiRouter.use(async (req, res, next) => {
   const prefix = 'Bearer ';
   const auth = req.header('Authorization');
-  if (!auth) { // nothing to see here
+
+  if (!auth) {
+    // nothing to see here
     next();
   } else if (auth.startsWith(prefix)) {
     const token = auth.slice(prefix.length);
+
     try {
       const { id } = jwt.verify(token, JWT_SECRET);
+
       if (id) {
         req.user = await getUserById(id);
         next();
+      } else {
+        next({
+          name: 'AuthorizationHeaderError',
+          message: 'Authorization token malformed',
+        });
       }
     } catch ({ name, message }) {
       next({ name, message });
@@ -23,7 +32,7 @@ apiRouter.use(async (req, res, next) => {
   } else {
     next({
       name: 'AuthorizationHeaderError',
-      message: `Authorization token must start with ${ prefix }`
+      message: `Authorization token must start with ${prefix}`,
     });
   }
 });
