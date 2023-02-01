deb095a71b00f4649004ca89ee65fa3b6c51478a
Meanithhuon <huon.meanith@gmail.com>
2023-01-31 18:10:13 -0600

    rearrange middleware

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 require('dotenv').config();
  
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const express = require('express');
 const server = express();
+
 const morgan = require('morgan');
-const apiRouter = require('./api');
-
 server.use(morgan('dev'));
+
 server.use(express.json())
-server.use('/api', apiRouter);
-
-const { client } = require('./db');
-client.connect();
 
 
 
 
-server.listen(PORT, () => {
-  console.log('The server is up on port', PORT)
-});
+
+
+
 
 
 
@@ -30,6 +26,20 @@ server.use((req, res, next) => {
     console.log("<_____Body Logger END_____>");
   
     next();
+  });
+
+
+  const apiRouter = require('./api');
+  server.use('/api', apiRouter);
+
+
+
+
+  const { client } = require('./db');
+client.connect();
+
+  server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
   });
 
 
